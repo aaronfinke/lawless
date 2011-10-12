8a0af162dee805261e66fa657bfae443b869c4f7
rmk65 <rmk65>
2011-10-12 13:29:20 +0000

    Use omp_get_num_procs() to determine the maxprocs value

--- controls.cpp
+++ controls.cpp
@@ -220,10 +220,11 @@ namespace scala
     nprocs = 1;
 #if _OPENMP
     //  Find out how many we are allowed
-    maxprocs = 0;
-    if (getenv("OMP_NUM_THREADS") != NULL) {
-      std::stringstream(std::string(getenv("OMP_NUM_THREADS"))) >> maxprocs;
-    }
+    //maxprocs = 0;
+    //if (getenv("OMP_NUM_THREADS") != NULL) {
+    //  std::stringstream(std::string(getenv("OMP_NUM_THREADS"))) >> maxprocs;
+    //}
+    maxprocs=omp_get_num_procs();
     if (fproc > 0.99) {
       // Explicitly set
       nprocs = Min(Nint(fproc), maxprocs); // reset to maximum if greater
